46c4d9529ad9cb8733e9e99b5fca289263465f28
tujiao-zh <z978766951@gmail.com>
2020-08-14 07:17:56 +0800

    create dotfiles

---
+++ .config/i3/feh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while true; do
+    find ~/Pictures/wallpaper/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |shuf -n1 -z | xargs -0 feh --bg-fill
+    sleep 5m
+done
---
+++ .config/picom/launch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin
+picom --experimental-backends -b &
+
